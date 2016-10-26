c9fe457ffda3c9223d6c1c625581db005ed9a26d
Brett <lathobravo957@rocketmail.com>
2016-10-26 13:00:07 +1100

    Add files via upload

---
+++ TreadLightly/include/CameraController.h
@@ -0,0 +1,31 @@
+/*
+* TREAD LIGHTLY
+* Authors: Brett Latham (26/10/16)
+* File: cameraController.h
+* Last Edited on: 26/10/16
+* Purpose: This file describes the movement of the camera in the scene.
+*/
+
+
+#include "stdafx.h"
+
+namespace TreadLightly {
+	class CameraController {
+	private:
+		CameraController(Ogre::Camera* cam);
+		virtual ~CameraController();
+	public:
+		virtual void Ogre::Camera* getCamera();
+		virtual void Ogre::SceneNode* getCameraNode();
+		virtual bool frameRenderingQueued(const Ogre::FrameEvent& evt);
+		virtual void mouseMoved(const OIS::MouseEvent& evt);
+		virtual void mousePressed(const (OIS::MouseEvent& evt);
+		virtual Ogre::Ray getSelectRay(){
+			return mCamera->getCameraToViewPortRay(mMouse->getMouseState().X.abs, mMouse->getMouseState().Y.abs);
+		}
+		Ogre::Camera * mCamera;
+		Ogre::SceneNode * mCameraNode;
+
+		OIS::Mouse * mMouse;
+	}
+}
